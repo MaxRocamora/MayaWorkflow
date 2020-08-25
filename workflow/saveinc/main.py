76ed01e6282e7a587c90dd0f2410b5c82b9f7e7c
MaxRocamora <maxirocamora@gmail.com>
2020-08-25 00:07:50 -0300

    added empty scene check

--- workflow/saveinc/main.py
+++ workflow/saveinc/main.py
@@ -7,6 +7,9 @@ import maya.cmds as cmds
 
 def save_incremental():
     ''' saves maya incremental '''
+    if not cmds.file(q=1, sn=1):
+        return
+
     print("Saving Incremental")
     cmds.file(modified=True)
     mel.eval("incrementAndSaveScene 0;")
