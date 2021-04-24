330bb1a1e489e6511609cc11796d327c603761bc
MaxRocamora <maxirocamora@gmail.com>
2021-04-24 13:06:01 -0300

    version update

--- workflow/reference_panel/version.py
+++ workflow/reference_panel/version.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 # Author: Maximiliano Rocamora / maxirocamora@gmail.com
-# Mini Panel for Maya Reference control
+# Maya Reference UI Picker
 
 '''
 1.0.0 : first version 06/2018
 1.1.0 : arcaneQt
 1.2.0 : removed arcaneQt
+1.5.0 : reworked ui, snakecase methods, minor refactor
 '''
 
 VERSION_MAJOR = 1
-VERSION_MINOR = 2
+VERSION_MINOR = 5
 VERSION_PATCH = 0
 
 version = '{}.{}.{}'.format(VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH)
 __version__ = version
 
-__app__ = 'RP'
+__app__ = 'RefUiPicker'
 __qt__ = 'Arcane:Qt_' + __app__ + '_ui'
 
 __all__ = ['version', '__version__', '__app__', '__qt__']
