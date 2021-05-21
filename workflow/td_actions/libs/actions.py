35364a17243da7a74f8170bcad200a4c8b72d40a
Maximilliano Rocamora <maxirocamora@gmail.com>
2021-05-21 19:16:40 -0300

    more methods added

--- workflow/td_actions/libs/actions.py
+++ workflow/td_actions/libs/actions.py
@@ -180,45 +180,144 @@ def delete_custom_attributes():
 # freeze transformations
 # center_pivot
 # clear_combine_selection
-# clean Separate
-# clean duplicate
-# copy Pivot
+# clean_separate
+# copy_pivot
 # restore shapes names
 # --------------------------------------------------------------------------------------------
 
-    def freeze_transformations():
-        ''' freezes transformations of selection  '''
-        sel = cmds.ls(sl=True)
-        if cmds.nodeType(sel) == "mesh":
-            all_sel = cmds.listRelatives(sel, parent=True, fullPath=False)
 
-        if all_sel is None:
-            return "Select Meshes first.", 0
+def freeze_transformations():
+    ''' freezes transformations of selection  '''
+    sel = cmds.ls(sl=True)
+    if cmds.nodeType(sel) == "mesh":
+        all_sel = cmds.listRelatives(sel, parent=True, fullPath=False)
 
-        for each in all_sel:
-            cmds.FreezeTransformations(each)
-        return "Transformations freezed on ({}) transform nodes".format(
+    if all_sel is None:
+        return "Select Meshes first.", 0
+
+    for each in all_sel:
+        cmds.FreezeTransformations(each)
+    return "Transformations freezed on ({}) transform nodes".format(
+        len(all_sel)), 1
+
+
+def center_pivot():
+    ''' reset pivot of transforms meshes '''
+    sel = cmds.ls(sl=True)
+    if sel is None:
+        return "Select Meshes to center pivot first.", 0
+    for each in sel:
+        if cmds.nodeType(each) == "transform":
+            cmds.CenterPivot(each)
+    return "Pivot Centered on ({}) transform nodes".format(len(sel))
+
+
+def clear_combine_selection():
+    ''' combines selection and deletes history '''
+    sel = cmds.ls(sl=True)
+    meshes = cmds.listRelatives(sel, pa=True, type="mesh")
+    all_sel = cmds.listRelatives(meshes, parent=True, fullPath=False)
+    if all_sel is None:
+        return "Select Meshes first.", 0
+    try:
+        cmds.polyUnite(sel, ch=False)  # combines and deletehistory
+    except RuntimeError as e:
+        return str(e), 0
+    return "Shapes combined with no history: ({})".format(len(all_sel)), 1
+
+
+def cleans_separate():
+    ''' Separates selection and deletes history '''
+    sel = cmds.ls(sl=True)
+    mesh = cmds.listRelatives(sel, pa=True, type="mesh")
+    all_sel = cmds.listRelatives(mesh, parent=True, fullPath=False)
+    if all_sel is None:
+        return "Select Meshes first.", 0
+    try:
+        cmds.polySeparate(sel, ch=False)  # combines and deletehistory
+        return "Shapes separated with no history: ({})".format(
             len(all_sel)), 1
+    except Exception as e:
+        return str(e), 0
 
-    def center_pivot(self):
-        ''' reset pivot of transforms meshes '''
-        sel = cmds.ls(sl=True)
-        if sel is None:
-            return "Select Meshes to center pivot first.", 0
-        for each in sel:
-            if cmds.nodeType(each) == "transform":
-                cmds.CenterPivot(each)
-        return "Pivot Centered on ({}) transform nodes".format(len(sel))
 
-    def clear_combine_selection(self):
-        ''' combines selection and deletes history '''
-        sel = cmds.ls(sl=True)
-        meshes = cmds.listRelatives(sel, pa=True, type="mesh")
-        all_sel = cmds.listRelatives(meshes, parent=True, fullPath=False)
-        if all_sel is None:
-            return "Select Meshes first.", 0
+def copy_pivot():
+    ''' Copy the pivot from source mesh to others objects. '''
+    if len(cmds.ls(sl=True)) < 2:
+        return "Select 2 or more objects.", 0
+
+    source_obj = cmds.ls(sl=True)[0]
+    target_objs = cmds.ls(sl=True)[0:-1]
+    parents = []
+    for obj in target_objs:
+        if cmds.listRelatives(obj, parent=True):
+            parents.append(cmds.listRelatives(obj, parent=True)[0])
+        else:
+            parents.append('')
+    pivot = cmds.xform(source_obj, q=True, ws=True, rotatePivot=True)
+    cmds.parent(target_objs, source_obj)
+    cmds.makeIdentity(target_objs, a=True, t=True, r=True, s=True)
+    cmds.xform(target_objs, ws=True, pivots=pivot)
+    for index, item in enumerate(target_objs):
+        if parents[index] != '':
+            cmds.parent(target_objs[index], parents[index])
+        else:
+            cmds.parent(target_objs[index], world=True)
+
+    return "Pivot Copied", 1
+
+
+def restore_shapes_names():
+    ''' resets shape names for selection '''
+    sel = cmds.ls(sl=True, type="mesh", dag=True, noIntermediate=True)
+    meshes = cmds.listRelatives(sel, parent=True, fullPath=1, path=True)
+
+    if meshes is None:
+        return 'Nothing Selected', 0
+
+    count = 0
+    base_shape_name = False
+    for index, mesh in enumerate(meshes):
+        shape_relatives = cmds.listRelatives(mesh, pa=True, type="shape")
+        if len(shape_relatives) == 1:
+            base_shape_name = shape_relatives[0]
+        else:
+            # If more than one shape is connected
+            # take only the shape with .inMesh' connection.
+            for shape in shape_relatives:
+                connections = cmds.listConnections(shape, sh=1, c=1)
+                if connections is None:
+                    continue
+
+                for con in connections:
+                    if '.inMesh' in con:
+                        base_shape_name = shape
+
+        # skip if we cannot resolve base_shape_name
+        if not base_shape_name:
+            continue
+
+        valid_shape_name = str(mesh) + "Shape"
+        # skip if the name are already valid
+        if base_shape_name == valid_shape_name:
+            continue
+
+        # warning on unicode error and name clashing
         try:
-            cmds.polyUnite(sel, ch=False)  # combines and deletehistory
-        except RuntimeError as e:
-            return str(e), 0
-        return "Shapes combined with no history: ({})".format(len(all_sel)), 1
+            new_shape_name = str(mesh) + "Shape"
+        except UnicodeEncodeError:
+            cmds.warning('UnicodeError on {}'.format(mesh), 0)
+            continue
+
+        if "|" in new_shape_name:
+            cmds.warning("WARNING: Nameclash on: ", new_shape_name)
+            continue
+
+        # rename and count
+        if base_shape_name != new_shape_name:
+            count += 1
+            cmds.rename(base_shape_name, new_shape_name)
+
+    msg = "Shapes Renamed in ( {} ) of ( {} ) Meshes.".format(
+        str(count), len(meshes))
+    return msg, 1
